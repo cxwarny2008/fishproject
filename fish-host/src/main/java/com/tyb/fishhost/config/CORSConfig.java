ce7c2f04298d02d01509d99ab7bac22c0ed2f8f8
陈小伟 <Garry.X.Chen@yzw.cn>
2018-08-21 16:29:04 +0800

    过时类WebMvcConfigurerAdapter替换

--- fish-host/src/main/java/com/tyb/fishhost/config/CORSConfig.java
+++ fish-host/src/main/java/com/tyb/fishhost/config/CORSConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import static org.springframework.web.cors.CorsConfiguration.ALL;
 
@@ -13,7 +12,7 @@ public class CORSConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
